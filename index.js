1322aa0caef3829503bfb786371352e86e3098e9
heejaykong <heejay.dev@gmail.com>
2020-11-07 00:44:55 +0900

    add font

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const mytitle = document.querySelector("#mytitle");
+
+const CLICKED_CLASS = "clicked"
+
+function handleClick(){
+    mytitle.classList.toggle(CLICKED_CLASS);
+}
+function init(){
+    mytitle.addEventListener("click", handleClick);
+}
+
+init();
+
+// ########################################
+
 function drawbg() {
     const string = "images/" + index + ".jpg";
     document.body.style.backgroundImage = `url(${string})`;
@@ -5,13 +20,13 @@ function drawbg() {
 }
 
 let index = 1;
-setInterval(function() {
-    if(index<=8) {
-        drawbg();
-        index = index + 1;
-    } else {
-        index = 1;
-        drawbg();
-        index = index + 1;
-    }
-}, 1000);
+// setInterval(function() {
+//     if(index<=8) {
+//         drawbg();
+//         index = index + 1;
+//     } else {
+//         index = 1;
+//         drawbg();
+//         index = index + 1;
+//     }
+// }, 3000);
